ad98cadf3a65f80e6fa5f5798c5cbf6e31152a70
Ariyan Ahmed <akkasislam05@gmail.com>
2026-03-12 23:55:36 +0600

    Fix vite base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Hello Akkas Islam! React চলছে 🚀</h1>
+      <h1>Hello MD: Akkas Islam! React চলছে 🚀</h1>
     </div>
-  );
+  );  
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: "/React/"
+  base: "React"   // এখানে অবশ্যই আপনার রিপোজিটরির নাম দিন
 })
